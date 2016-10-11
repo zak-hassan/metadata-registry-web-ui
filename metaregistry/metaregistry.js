64c01960d089b4069090a9d68489d0dbaddbf496
Zak Hassan <zak.hassan1010@gmail.com>
2016-10-11 18:02:16 -0400

    Updated ui

--- metaregistry/metaregistry.js
+++ metaregistry/metaregistry.js
@@ -8,7 +8,38 @@ angular.module('Registry.metaregistry', ['ngRoute'])
     controller: 'MetaRegistryCtrl'
   });
 }])
+.factory("SchemaService",function(){
+  var schemas=[
+    { schemaId: "24190df4-e7fd-4a05-b0d9-a3b384c076e3", type: "a",name: "a", namespace: "a", doc: "a", fields: [ { name: "occupation", type: "string" }, { name: "name", type: "string" }, { name: "age", type: "string" } ] }
+  ];
 
-.controller('MetaRegistryCtrl', [function() {
+  var getSchemas = function() {
+    return schemas;
+  };
+
+  return {
+    getSchemas: getSchemas
+  };
+
+})
+.controller('MetaRegistryCtrl',['SchemaService', function($scope, SchemaService) {
+
+$scope.name=SchemaService.getSchemas();
+
+
+// $rootScope.getSchemas(function(){
+//
+//   $http({
+//     method: 'GET',
+//     url: '/api/schema'
+//   }).then(function successCallback(response) {
+//       // this callback will be called asynchronously
+//       // when the response is available
+//     }, function errorCallback(response) {
+//       // called asynchronously if an error occurs
+//       // or server returns response with an error status.
+//     });
+// }]);
+
 
 }]);
